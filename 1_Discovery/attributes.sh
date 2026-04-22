5c0b5951feb42744925f77b743cfcc9bad867e8f
Kernchen, Sophie <sophie.kernchen@dlr.de>
2026-04-22 20:01:07 +0200

    Discovery with Mi Band 5

--- 1_Discovery/attributes.sh
+++ 1_Discovery/attributes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mac="E0:B2:9B:3E:45:F2"
-tmux new-session -d -s test 'gatttool -I -b E0:B2:9B:3E:45:F2';
+tmux new-session -d -s test "gatttool -I -b $mac";
 tmux send 'connect' ENTER;
 sleep 2;
 tmux send 'primary' ENTER;
--- 1_Discovery/list-attributes.sh
+++ 1_Discovery/list-attributes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mac="E0:B2:9B:3E:45:F2"
 tmux new-session -d -s list-attr 'bluetoothctl';
-tmux send 'connect E0:B2:9B:3E:45:F2' ENTER;
+tmux send "connect $mac" ENTER;
 sleep 3;
-tmux send 'pair E0:B2:9B:3E:45:F2' ENTER;
+tmux send "pair $mac" ENTER;
 sleep 1;
 tmux send 'menu gatt' ENTER;
 tmux send 'list-attributes' ENTER;
